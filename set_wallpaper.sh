e8991df0f47e71d5b4aca26e33f7af0fed87d7e7
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2024-02-20 11:21:30 -0300

    update python script to remove the need for set_wallpaper script

--- set_wallpaper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-python3 $(pwd)/what_a_year.py
-
-
-gsettings set org.gnome.desktop.background picture-uri-dark $(pwd)/current_year.png
-gsettings set org.gnome.desktop.background picture-uri $(pwd)/current_year.png
