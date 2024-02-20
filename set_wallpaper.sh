96f53345ea5b198ee060bf663d5718614666a8c5
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2024-02-20 13:14:53 -0300

    fix gsettings bug'

--- set_wallpaper.sh
+++ set_wallpaper.sh
@@ -7,7 +7,7 @@ git pull origin main
 
 python3 what_a_year.py
 
-gsettings set org.gnome.desktop.background picture-uri-dark {os.getcwd()}/current_date.png
-gsettings set org.gnome.desktop.background picture-uri {os.getcwd()}/current_date.png
+gsettings set org.gnome.desktop.background picture-uri-dark $(pwd)/current_date.png
+gsettings set org.gnome.desktop.background picture-uri $(pwd)/current_date.png
 
 cd $CURRENT_PATH
